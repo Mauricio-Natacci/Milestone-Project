d94e25342b4b67bf68b74ee5fa9f7acfb1c9a027
Mauricio-Natacci <85077684+Mauricio-Natacci@users.noreply.github.com>
2022-08-03 21:02:17 -0300

    rendered the first view

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-const express = require('express')
+const path = require('path');
 
-const authRoutes = require('./routes/auth.routes')
+const express = require('express');
 
-const app = express()
+const authRoutes = require('./routes/auth.routes');
 
-app.use(authRoutes)
+const app = express();
 
-app.listen(3000)
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+
+app.use(authRoutes);
+
+app.listen(3000);
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,11 +1,11 @@
-const express = require('express')
+const express = require('express');
 
-const authController = require('../controllers/auth.controller')
+const authController = require('../controllers/auth.controller');
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/signup', authController.getSignup)
+router.get('/signup', authController.getSignup);
 
-router.get('/login', authController.getLogin)
+router.get('/login', authController.getLogin);
 
-module.exports = router
+module.exports = router;
