3c323465cbf724307737422ef1519d81ec2f434f
Mauricio-Natacci <85077684+Mauricio-Natacci@users.noreply.github.com>
2022-08-12 22:36:40 -0300

    Adding the Product Details page

--- models/product.model.js
+++ models/product.model.js
@@ -1,3 +1,5 @@
+const mongodb = require('mongodb')
+
 const db = require('../data/database')
 
 class Product {
@@ -6,9 +8,42 @@ class Product {
     this.summary = productData.summary
     this.price = +productData.price
     this.description = productData.description
-    this.image = productData.image //the name of the image file
+    this.image = productData.image // the name of the image file
     this.imagePath = `product-data/images/${productData.image}`
     this.imageUrl = `/products/assets/images/${productData.image}`
+    if (productData._id) {
+      this.id = productData._id.toString()
+    }
+  }
+
+  static async findById(productId) {
+    let prodId
+    try {
+      prodId = new mongodb.ObjectId(productId)
+    } catch (error) {
+      error.code = 404
+      throw error
+    }
+    const product = await db
+      .getDb()
+      .collection('products')
+      .findOne({ _id: prodId })
+
+    if (!product) {
+      const error = new Error('Could not find product with provided id.')
+      error.code = 404
+      throw error
+    }
+
+    return new Product(product)
+  }
+
+  static async findAll() {
+    const products = await db.getDb().collection('products').find().toArray()
+
+    return products.map(function (productDocument) {
+      return new Product(productDocument)
+    })
   }
 
   async save() {
@@ -20,7 +55,22 @@ class Product {
       image: this.image
     }
 
-    await db.getDb().collection('products').insertOne(productData)
+    if (this.id) {
+      const productId = new mongodb.ObjectId(this.id)
+
+      if (!this.image) {
+        delete productData.image
+      }
+
+      await db.getDb().collection('products').updateOne(
+        { _id: productId },
+        {
+          $set: productData
+        }
+      )
+    } else {
+      await db.getDb().collection('products').insertOne(productData)
+    }
   }
 }
 
