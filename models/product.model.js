7a7ae80cfe4c67b7270f64cafaa8b476a626bd2e
Mauricio-Natacci <85077684+Mauricio-Natacci@users.noreply.github.com>
2022-08-20 16:11:18 -0300

    finished!

--- models/product.model.js
+++ models/product.model.js
@@ -1,53 +1,69 @@
-const mongodb = require('mongodb')
+const mongodb = require('mongodb');
 
-const db = require('../data/database')
+const db = require('../data/database');
 
 class Product {
   constructor(productData) {
-    this.title = productData.title
-    this.summary = productData.summary
-    this.price = +productData.price
-    this.description = productData.description
-    this.image = productData.image // the name of the image file
-    this.updateImageData()
+    this.title = productData.title;
+    this.summary = productData.summary;
+    this.price = +productData.price;
+    this.description = productData.description;
+    this.image = productData.image; // the name of the image file
+    this.updateImageData();
     if (productData._id) {
-      this.id = productData._id.toString()
+      this.id = productData._id.toString();
     }
   }
 
   static async findById(productId) {
-    let prodId
+    let prodId;
     try {
-      prodId = new mongodb.ObjectId(productId)
+      prodId = new mongodb.ObjectId(productId);
     } catch (error) {
-      error.code = 404
-      throw error
+      error.code = 404;
+      throw error;
     }
     const product = await db
       .getDb()
       .collection('products')
-      .findOne({ _id: prodId })
+      .findOne({ _id: prodId });
 
     if (!product) {
-      const error = new Error('Could not find product with provided id.')
-      error.code = 404
-      throw error
+      const error = new Error('Could not find product with provided id.');
+      error.code = 404;
+      throw error;
     }
 
-    return new Product(product)
+    return new Product(product);
   }
 
   static async findAll() {
-    const products = await db.getDb().collection('products').find().toArray()
+    const products = await db.getDb().collection('products').find().toArray();
 
     return products.map(function (productDocument) {
-      return new Product(productDocument)
+      return new Product(productDocument);
+    });
+  }
+
+  static async findMultiple(ids) {
+    const productIds = ids.map(function(id) {
+      return new mongodb.ObjectId(id);
     })
+    
+    const products = await db
+      .getDb()
+      .collection('products')
+      .find({ _id: { $in: productIds } })
+      .toArray();
+
+    return products.map(function (productDocument) {
+      return new Product(productDocument);
+    });
   }
 
   updateImageData() {
-    this.imagePath = `product-data/images/${this.image}`
-    this.imageUrl = `/products/assets/images/${this.image}`
+    this.imagePath = `product-data/images/${this.image}`;
+    this.imageUrl = `/products/assets/images/${this.image}`;
   }
 
   async save() {
@@ -56,37 +72,36 @@ class Product {
       summary: this.summary,
       price: this.price,
       description: this.description,
-      image: this.image
-    }
+      image: this.image,
+    };
 
     if (this.id) {
-      const productId = new mongodb.ObjectId(this.id)
+      const productId = new mongodb.ObjectId(this.id);
 
       if (!this.image) {
-        delete productData.image
+        delete productData.image;
       }
 
       await db.getDb().collection('products').updateOne(
         { _id: productId },
         {
-          $set: productData
+          $set: productData,
         }
-      )
+      );
     } else {
-      await db.getDb().collection('products').insertOne(productData)
+      await db.getDb().collection('products').insertOne(productData);
     }
   }
 
-
-  replaceImage(newImage){
-    this.image = newImage
-    this.updateImageData()
+  replaceImage(newImage) {
+    this.image = newImage;
+    this.updateImageData();
   }
 
-   remove() {
-    const productId = new mongodb.ObjectId(this.id)
-    return db.getDb().collection('products').deleteOne({_id: productId})
+  remove() {
+    const productId = new mongodb.ObjectId(this.id);
+    return db.getDb().collection('products').deleteOne({ _id: productId });
   }
 }
 
-module.exports = Product
+module.exports = Product;
