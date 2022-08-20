d966f10d05a2346eaef555048c43d61e14ff3255
Mauricio-Natacci <85077684+Mauricio-Natacci@users.noreply.github.com>
2022-08-19 23:14:11 -0300

    updating carts via ajax requests

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -1,30 +1,48 @@
-const Product = require('../models/product.model');
+const Product = require('../models/product.model')
 
 function getCart(req, res) {
   res.render('customer/cart/cart')
 }
 
 async function addCartItem(req, res, next) {
-  let product;
+  let product
   try {
-    product = await Product.findById(req.body.productId);
+    product = await Product.findById(req.body.productId)
   } catch (error) {
-    next(error);
-    return;
+    next(error)
+    return
   }
 
-  const cart = res.locals.cart;
+  const cart = res.locals.cart
 
-  cart.addItem(product);
-  req.session.cart = cart;
+  cart.addItem(product)
+  req.session.cart = cart
 
   res.status(201).json({
     message: 'Cart updated!',
     newTotalItems: cart.totalQuantity
-  });
+  })
+}
+
+function updateCartItem(req, res) {
+  const cart = res.locals.cart
+
+  const updatedItemData = cart.updateItem(req.body.productId, req.body.quantity)
+
+  req.session.cart = cart
+
+  res.json({
+    message: 'Item updated!',
+    updatedCartData: {
+      newTotalQuantity: cart.totalQuantity,
+      newTotalPrice: cart.totalPrice,
+      updatedItemPrice: updatedItemData.updatedItemPrice
+    }
+  })
 }
 
 module.exports = {
   addCartItem: addCartItem,
-  getCart: getCart
-};
+  getCart: getCart,
+  updateCartItem: updateCartItem
+}
--- routes/cart.routes.js
+++ routes/cart.routes.js
@@ -1,11 +1,13 @@
-const express = require('express');
+const express = require('express')
 
-const cartController = require('../controllers/cart.controller');
+const cartController = require('../controllers/cart.controller')
 
-const router = express.Router();
+const router = express.Router()
 
 router.get('/', cartController.getCart) // /cart/
 
-router.post('/items', cartController.addCartItem); // /cart/items
+router.post('/items', cartController.addCartItem) // /cart/items
 
-module.exports = router;
+router.patch('/items', cartController.updateCartItem)
+
+module.exports = router
